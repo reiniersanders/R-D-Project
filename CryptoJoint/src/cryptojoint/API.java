e667e223ee6370d067660f3e1a558c9b4ba00b36
laurensKubat <laurensKubat@gmail.com>
2018-05-18 15:54:04 +0200

    implemented API.makecall()

--- CryptoJoint/src/cryptojoint/API.java
+++ CryptoJoint/src/cryptojoint/API.java
@@ -5,12 +5,41 @@
  */
 package cryptojoint;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 /**
  *
  * @author Laurens
  */
 public class API {
     private String pair;
-    private String exchange;
+    private String url;
     
+    public API(String pair, String url){
+        this.pair = pair;
+        this.url = url;
+    }
+    
+    public String getPair(){
+        return this.pair;
+    }
+    
+    //Makes a Get request
+    public String makeCall() throws UnsupportedEncodingException, MalformedURLException, IOException{
+        String charset = "UTF-8";
+        String symbol = this.pair;
+
+        String query = String.format("symbol=%s", 
+        URLEncoder.encode(symbol, charset));
+        URLConnection connection = new URL(url + "?" + query).openConnection();
+        connection.setRequestProperty("Accept-Charset", charset);
+        InputStream response = connection.getInputStream();
+        return response.toString();
+    } 
 }
