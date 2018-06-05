af3ceb9dbaaab10b3e59591ba574240ae4edd466
FerranvanderHave <39337173+FerranvanderHave@users.noreply.github.com>
2018-06-05 15:09:31 +0200

    Update Trade.java

--- CryptoJoint/Trade.java
+++ CryptoJoint/Trade.java
@@ -3,61 +3,117 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package traderapp;
+package trader.app;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Ferran
  */
 public class Trade {
+    public boolean buy;
     public Trader trader;
-    public Currency start;
-    public Currency target;
+    public CurrencyTuple currencytuple;
     public double amount;
     public double targetvalue;
     public double amounttarget;
     public int id;
-    
-    public Trade(Trader trader, Currency start, double amount, Currency target, double targetvalue, int id)
+    /**
+     * Constructor of Trade
+     * 
+     * @param buy           Indicates whether it is a buy or sell trade.
+     *                          true if it is a buy trade.
+     *                          false if it is a sell trade.
+     * @param trader        The trader which conducts this trade.
+     * @param start         The currency which you are going to trade.  
+     * @param amount        The amount of the currency which you are going to trade.
+     * @param target        The currency you want to get after this trade.
+     * @param targetvalue   The value at which you want to conduct the trade.
+     *                          If this is equal to 0 conduct the trade at the next update.
+     * @param id            The trade id. This is unique for every trade.
+     */
+    public Trade(boolean buy, Trader trader, Currency start, double amount, Currency target, double targetvalue, int id)
     {
+        this.buy=buy;
         this.trader=trader;
-        this.start=start;
+        this.currencytuple=new CurrencyTuple(start, target);
         this.amount=amount;
-        this.target=target;
         this.targetvalue=targetvalue;
         this.id=id;
     }
-    
+    /**
+     * Updates the values of the two currencies in this trade.
+     */
+    public void updateValues()
+    {
+        ArrayList<CurrencyTuple> currencies = trader.getCurrencyList();
+        for (CurrencyTuple currency: currencies)
+        {
+            if (currencytuple.getOwned().getName().equals(currency.getOwned().getName()) && 
+                currencytuple.getNotOwned().getName().equals(currency.getNotOwned().getName()))
+                currencytuple.setPrice(currency.getPrice());
+        }
+    }
+    /**
+     * Simple getter.
+     * @return  The id of the trade.
+     */
     public int getId()
     {
         return id;
     }
     
-    public double currencyAmount()
+    private double currencyAmount()
     {
-        return (start.getValue()*amount)/(target.getValue());
+        return (currencytuple.getPrice()*amount);
     }
-    
+    /**
+     * Conducts the trade if the circumstances allow it.
+     * @return  True if the trade has been conducted succesfully.
+     *          False if the trade has not been conducted.
+     */
     public boolean doTrade()
     {
-        if (targetvalue==0)
+        if (buy);
         {
-            trader.addWallet(target, currencyAmount());
-            trader.removeTrade(this);
-            return true;
+            if (targetvalue==0)
+            {
+                trader.addWallet(currencytuple.getNotOwned(), currencyAmount());
+                trader.removeTrade(this);
+                return true;
+            }
+            if (currencytuple.getPrice()<= targetvalue)
+            {
+                trader.addWallet(currencytuple.getNotOwned(), currencyAmount());
+                trader.removeTrade(this);
+                return true;
+            }
         }
-        if (target.getValue()<= targetvalue)
+        if (!buy)
         {
-            trader.addWallet(target, currencyAmount());
-            trader.removeTrade(this);
-            return true;
+            if (targetvalue==0)
+            {
+                trader.addWallet(currencytuple.getNotOwned(), currencyAmount());
+                trader.removeTrade(this);
+                return true;
+            }
+            if (currencytuple.getPrice()<= targetvalue)
+            {
+                trader.addWallet(currencytuple.getNotOwned(), currencyAmount());
+                trader.removeTrade(this);
+                return true;
+            }
         }
         return false;
     }
-    
+    /**
+     * Simple toString.
+     * @return The trade in string format.
+     */
     @Override
     public String toString()
     {
-        return amount + " " + start + " for " + target + " with id: " + id;
+        return amount + " " + currencytuple.getOwned() + " for " + currencytuple.getNotOwned() + " with id: " + id;
     }
 }
