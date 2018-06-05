e374d26b26732887182421bd7e8704135d97562d
laurensKubat <laurensKubat@gmail.com>
2018-06-05 10:49:53 +0200

    Added Javadoc

--- CryptoJoint/src/cryptojoint/APIFetcher.java
+++ CryptoJoint/src/cryptojoint/APIFetcher.java
@@ -5,24 +5,40 @@
  */
 package cryptojoint;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  *
  * @author Laurens
  */
 public class APIFetcher {
+    private APIBinance binance;
     
     public APIFetcher(){
     }
     
-    public APIBinance GetFormat(Currency toBuy, Currency toSell, String Exchange){
+    /**
+     *
+     * @param toBuy The currency which is to be bought
+     * @param toSell The currency to be sold
+     * @param Exchange the Exchange on which to buy/sell
+     * @return object which inherits API
+     */
+    public API GetFormat(Currency toBuy, Currency toSell, String Exchange){
         switch (Exchange) {
             case "Binance": return MakeBinanceAPI((toSell.getName() + toBuy.getName()));
             default: return null;
         }
         
+    }
+    
+    public ArrayList<CurrencyTuple> getSymbols(String Exchange) throws IOException{
+        switch (Exchange) {
+            case "Binance": return binance.getSymbols();
+        }
+        return null;
     }
     
     public APIBinance MakeBinanceAPI(String Endpoint){
