ec229f2497c5e8aa98ce9463b761538953226413
laurensKubat <laurensKubat@gmail.com>
2018-06-05 10:38:54 +0200

    Added Javadoc

--- CryptoJoint/src/cryptojoint/Updater.java
+++ CryptoJoint/src/cryptojoint/Updater.java
@@ -25,12 +25,20 @@ public class Updater {
     
     
 
-    
-    //only binance supported at the moment
+   /**
+    * Returns an ArrayList of CurrencyTuple which contains the the price and two
+    * currency objects, the price is how many notOwned Currencies the buyer gets
+    * for one owned Currency
+    * @param toUpdate    an ArrayList of all currencyTuples which need an update
+    * @param Exchange    The exchange to use, A capitalized String
+    * @return               An ArrayList of CurrencyTuple
+     * @throws java.net.MalformedURLException wrong url, probably means symbols
+     * in wrong order, Caller has to catch this
+    */
     public ArrayList<CurrencyTuple> GetUpdate(ArrayList<CurrencyTuple> toUpdate, String Exchange) throws MalformedURLException, IOException{
-        ArrayList<CurrencyTuple> returnArray = new ArrayList<CurrencyTuple>();
+        ArrayList<CurrencyTuple> returnArray = new ArrayList<>();
         for (int i = 0; i < toUpdate.size(); i++) {
-            APIBinance api = fetcher.GetFormat(toUpdate.get(i).getOwned(),
+            API api = fetcher.GetFormat(toUpdate.get(i).getOwned(),
                         toUpdate.get(i).getNotOwned(), Exchange);
             Double result = api.makeCall();
             CurrencyTuple tupleBuffer = toUpdate.get(i);
