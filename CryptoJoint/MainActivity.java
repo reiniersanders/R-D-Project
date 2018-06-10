4932947f4b9a9fd96224d3b0f85eda42c32bdf8d
Reinier Sanders <34060491+reiniersanders@users.noreply.github.com>
2018-06-10 18:25:44 +0200

    Add files via upload

---
+++ CryptoJoint/MainActivity.java
@@ -0,0 +1,147 @@
+package com.pt04.cryptojoint;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    Client client;
+    Trader trader;
+    Updater updater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Trade", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        client = new Client(this);
+        updater = new Updater();
+        trader = new Trader(updater, client,"Binance");
+        client.setTrader(trader);
+
+        //kijkt of er data geladen is (en laad deze meteen)
+        if (!loadData()) {
+            Map<String, Double> wallet = new HashMap<>();
+            client.setWallet(new Wallet(wallet, 0.0, client));
+        }
+    }
+
+    /**
+     *  this gets called whenever new trade information is received and the views should be updated
+     */
+    public void updateViews(){
+        TextView currenciesView = findViewById(R.id.text_viewing_map);
+        TextView walletView = findViewById(R.id.text_holding_map);
+
+        client.printCurrencies(currenciesView);
+        System.out.println("");
+        client.getWallet().printWallet(walletView, "USDT");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //saveData();
+    }
+
+    /**
+     * @return returns if data is loaded successfully
+     */
+    public boolean loadData(){
+        //not finished yet
+        return false;
+        /*
+        //laad alle data die opgeslagen staat op het apparaat, als er niks staat return false
+        File file = new File(getDir("data", Context.MODE_PRIVATE), "wallet");
+        if (file.exists()) {
+            try {
+                ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
+                client.setCurrencies((List<Currency>) inputStream.readObject());
+                client.setWallet((Wallet) inputStream.readObject());
+                inputStream.close();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;*/
+    }
+
+    /**
+     *  saves wallet and trader data to device
+     */
+    public void saveData(){
+        //not finished yet
+        File file = new File(getDir("data", Context.MODE_PRIVATE), "wallet");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                return;
+            }
+        }
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file));
+            outputStream.writeObject(client.getCurrencies());
+            outputStream.writeObject(client.getWallet());
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_client, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
